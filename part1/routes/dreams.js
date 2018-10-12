80cf665446e6d6672cd53f21ef5487e5dbedb093
Jai Aravindh <jaitvsmhss@gmail.com>
2018-10-12 20:57:52 +0000

    nineth Commit

--- part1/routes/dreams.js
+++ part1/routes/dreams.js
@@ -14,42 +14,56 @@ router.get("/", function(req, res){
     });
 });
 
-//CREATE - add new dreams to DB
-router.post("/", function(req, res){
+//CREATE - add new dream to DB
+router.post("/", isLoggedIn, function(req, res){
     // get data from form and add to dreams array
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
-    var newdreams = {name: name, image: image, description: desc}
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
+    var newdreams = {name: name, image: image, description: desc, author:author}
     // Create a new dream and save to DB
     dreams.create(newdreams, function(err, newlyCreated){
         if(err){
             console.log(err);
         } else {
             //redirect back to dreams page
+            console.log(newlyCreated);
             res.redirect("/dreams");
         }
     });
 });
 
-//NEW - show form to create new dreams
-router.get("/new", function(req, res){
+//NEW - show form to create new dream
+router.get("/new", isLoggedIn, function(req, res){
    res.render("dreams/new"); 
 });
 
 // SHOW - shows more info about one dream
 router.get("/:id", function(req, res){
-    //find the dreams with provided ID
+    //find the dream with provided ID
     dreams.findById(req.params.id).populate("comments").exec(function(err, founddreams){
         if(err){
             console.log(err);
         } else {
             console.log(founddreams)
-            //render show template with that dreams
+            //render show template with that dream
             res.render("dreams/show", {dreams: founddreams});
         }
     });
 });
+
+
+//middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 module.exports = router;
 
