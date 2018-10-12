5067e5b328938426e9419c7774527fae356f0198
Jai Aravindh <jaitvsmhss@gmail.com>
2018-10-12 15:40:22 +0000

    sixth Commit

--- part1/app.js
+++ part1/app.js
@@ -2,15 +2,35 @@ var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
     dreams  = require("./models/dreams"),
     Comment     = require("./models/comment"),
-    seedDB      = require("./seeds")
+    User        = require("./models/user"),
+    seedDB      = require("./seeds");
     
 mongoose.connect("mongodb://localhost/oneiro");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "nothing to say!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   next();
+});
 
 app.get("/", function(req, res){
     res.render("landing");
@@ -46,20 +66,20 @@ app.post("/dreams", function(req, res){
     });
 });
 
-//NEW - show form to create new dreams
+//NEW - show form to create new dream
 app.get("/dreams/new", function(req, res){
    res.render("dreams/new"); 
 });
 
-// SHOW - shows more info about one dreams
+// SHOW - shows more info about one dream
 app.get("/dreams/:id", function(req, res){
-    //find the dreams with provided ID
+    //find the dream with provided ID
     dreams.findById(req.params.id).populate("comments").exec(function(err, founddreams){
         if(err){
             console.log(err);
         } else {
             console.log(founddreams)
-            //render show template with that dreams
+            //render show template with that dream
             res.render("dreams/show", {dreams: founddreams});
         }
     });
@@ -70,8 +90,8 @@ app.get("/dreams/:id", function(req, res){
 // COMMENTS ROUTES
 // ====================
 
-app.get("/dreams/:id/comments/new", function(req, res){
-    // find dreams by id
+app.get("/dreams/:id/comments/new", isLoggedIn, function(req, res){
+    // find dream by id
     dreams.findById(req.params.id, function(err, dreams){
         if(err){
             console.log(err);
@@ -81,8 +101,8 @@ app.get("/dreams/:id/comments/new", function(req, res){
     })
 });
 
-app.post("/dreams/:id/comments", function(req, res){
-   //lookup dreams using ID
+app.post("/dreams/:id/comments",isLoggedIn,function(req, res){
+   //lookup dream using ID
    dreams.findById(req.params.id, function(err, dreams){
        if(err){
            console.log(err);
@@ -99,7 +119,58 @@ app.post("/dreams/:id/comments", function(req, res){
         });
        }
    });
+   //create new comment
+   //connect new comment to dream
+   //redirect dream show page
 });
+
+
+//  ===========
+// AUTH ROUTES
+//  ===========
+
+// show register form
+app.get("/register", function(req, res){
+   res.render("register"); 
+});
+//handle sign up logic
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+           res.redirect("/dreams"); 
+        });
+    });
+});
+
+// show login form
+app.get("/login", function(req, res){
+   res.render("login"); 
+});
+// handling login logic
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/dreams",
+        failureRedirect: "/login"
+    }), function(req, res){
+});
+
+// logic route
+app.get("/logout", function(req, res){
+   req.logout();
+   res.redirect("/dreams");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The Oneiro Server Has Started!");
